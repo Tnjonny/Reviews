735ccb106be306b5ac5a7d9febaf08f83f43ff11
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-02-22 21:19:15 +0000

    commit

---
+++ rev.js
@@ -0,0 +1,23 @@
+const reviews = [
+    {
+        id: 1,
+        name: "John Smith",
+        job: "web developer",
+        img: "#",
+        text: "lorem20"
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        job: "software developer",
+        img: "#",
+        text: "lorem20"
+    },
+    {
+        id: 3,
+        name: "Jonathan Nkuli",
+        job: "Merch Assistant",
+        img: "#",
+        text: "lorem20"
+    }
+]
