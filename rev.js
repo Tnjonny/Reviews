3b064e2337402db775d6f54c467ad953a6083245
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-02-22 21:26:29 +0000

    commit

--- rev.js
+++ rev.js
@@ -21,3 +21,13 @@ const reviews = [
         text: "lorem20"
     }
 ]
+
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querrySelector(".prev-btn");
+const nextBtn = document.querrySelector(".next-btn");
+const randomBtn = document.querrySelector(".random-btn");
